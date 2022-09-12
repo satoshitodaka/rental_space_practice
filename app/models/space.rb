3dd4a916edb8c277d8dcbd6811a09b9c06a0594c
satoshitodaka <satoshitodaka0705@gmail.com>
2022-09-12 19:06:18 +0900

    add spaceモデルを追加

---
+++ app/models/space.rb
@@ -0,0 +1,18 @@
+# == Schema Information
+#
+# Table name: spaces
+#
+#  id              :bigint           not null, primary key
+#  address         :string(255)
+#  description     :text(65535)      not null
+#  latitude        :float(24)
+#  longitude       :float(24)
+#  name            :string(255)      not null
+#  nearest_station :string(255)
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
+class Space < ApplicationRecord
+  validates :name, presence: true
+  validates :description, presence: true
+end
