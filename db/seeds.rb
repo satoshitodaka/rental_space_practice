d8a62c5be209a657f7e88cc61db84f79ad7b3ce9
satoshitodaka <satoshitodaka0705@gmail.com>
2022-09-13 11:26:45 +0900

    add ダミーデータを作成し、表示できるようにした

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,28 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+# space
+100.times.each do |n|
+  # ランダムの緯度経度を作成
+  latitude = Random.rand(35.6..35.7)
+  longitude = Random.rand(139.70..139.78)
+
+  space = Space.create!(
+    name: Faker::Lorem.sentence,
+    description: Faker::Lorem.paragraph(
+      sentence_count: 30,
+      supplemental: false,
+      random_sentences_to_add: 4,
+    ),
+    address: Faker::Address.full_address,
+    nearest_station: Faker::Address.city,
+    latitude: latitude,
+    longitude: longitude,
+  )
+
+  # 3.times.each do |n|
+  #   画像をランダムで取得するようにする
+  # end
+
+  # スペースの特徴についての処理
+
+
+
+end
